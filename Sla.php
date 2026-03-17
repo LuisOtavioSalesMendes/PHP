aba70e2cf802bf7604c91107cb7be3f9b3c85f19
LuisOtavioSalesMendes <qetiotee@gmail.com>
2026-03-17 11:37:17 -0300

    Add files via upload

---
+++ Sla.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de itens</title>
+</head>
+<body>
+    <h1>Cadastro de items</h1>
+    <form method="POST">
+        <label>Equipamento</label><br>
+        <input type="text" name="Equipamento"><br>
+
+        <label>Quantidade</label><br>
+        <input type="number" name="quantidade"><br>
+
+        <label>Local</label><br>
+        <input type="text" name="Local"><br>
+
+        
+</form>
+</body>
+</html>
